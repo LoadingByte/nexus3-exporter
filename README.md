d4904f6520c8c749a0d1c3b14830b3c94f6d1f57
LoadingByte <hello@loadingbyte.com>
2019-09-08 02:27:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Nexus 3 Exporter
+
+A little script for downloading all assets inside a Nexus 3 repository, following the the repository's asset format.
+For example, fed with a Nexus 3 Maven repositories, this script yields a standard Maven repository structure containing all artifacts.
+
+## Usage
+
+You need Python 3 to run this script. You also need to install the requirements listed in `requirements.txt` (e.g., using pip).
+
+Launch the script with:
+
+    python3 nexus3_exporter.py [-h] [-o output_dir] [-n] <server> <repo>
+
+You have to supply two arguments:
+
+* `server` is the root URL to the Nexus 3 server which contains the repository. For example, this might be `https://repo.loadingbyte.com`.
+* `repo` is the name of the repository whose assets shall be downloaded. For example, this might be `maven-releases`.
+
+Optionally, you can supply:
+
+* `-h` shows a help page.
+* `-o output_dir` specifies the directory where to store the downladed assets. If none is provided, the repository name will be used as the name of the output directory.
+* `-n` disables the SHA-1 hash verification of downloaded assets.
+
+Example call:
+
+    python3 nexus3_exporter.py https://repo.loadingbyte.com maven-releases
+
+# Importing
+
+This script only exports assets from Nexus 3 repositories, but not vice versa.
+For importing assets, we recommend using the [official import scripts](https://github.com/sonatype-nexus-community/nexus-repository-import-scripts).
---
+++ nexus3_exporter.py
@@ -0,0 +1,131 @@
+import argparse
+import hashlib
+import os
+from json.decoder import JSONDecodeError
+from urllib.parse import urljoin
+
+import requests
+from tqdm import tqdm
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A little script for downloading all assets inside a Nexus 3 repository, "
+                    "following the the repository's format (e.g., Maven 2).")
+    parser.add_argument("server",
+                        help="Root URL to Nexus 3 server; e.g., https://repo.loadingbyte.com")
+    parser.add_argument("repo",
+                        help="Name of repository whose assets shall be downloaded; e.g., maven-releases")
+    parser.add_argument("-o", metavar="output_dir", dest="output_dir",
+                        help="Directory where to store the downladed assets; "
+                             "if none is provided, the repository name will be used.")
+    parser.add_argument("-n", dest="no_verify", action="store_true",
+                        help="Disable the SHA-1 hash verification of downloaded assets.")
+
+    args = parser.parse_args()
+    server_url = args.server
+    repo_name = args.repo
+    output_dir = args.output_dir
+    no_verify = args.no_verify
+
+    if not output_dir:
+        output_dir = repo_name
+    if os.path.exists(output_dir):
+        print(f"Output directory '{output_dir}' already exists. Please delete it and then re-run the script.")
+        abort(1)
+
+    if "://" not in server_url:
+        server_url = "http://" + server_url
+
+    print("Fetching asset listing...")
+    asset_listing = fetch_asset_listing(server_url, repo_name)
+    print("Done!")
+
+    print("Downloading and verifying assets...")
+    download_assets(output_dir, no_verify, asset_listing)
+    print("Done!")
+
+
+def abort(code):
+    print("Aborting script!")
+    exit(code)
+
+
+def fetch_asset_listing(server_url, repo_name):
+    asset_api_url = urljoin(server_url, f"service/rest/v1/assets?repository={repo_name}")
+
+    asset_listing = []
+    continuation_token = -1  # -1 is a special value hinting the first iteration
+
+    with tqdm(unit=" API requests") as pbar:
+        while continuation_token:
+            if continuation_token == -1:
+                query_url = asset_api_url
+            else:
+                query_url = f"{asset_api_url}&continuationToken={continuation_token}"
+
+            try:
+                resp = requests.get(query_url, verify=False).json()
+            except IOError as e:
+                pbar.close()
+                print(str(e))
+                abort(2)
+            except JSONDecodeError as e:
+                pbar.close()
+                print(f"Cannot decode JSON response. Are you sure that the server URL {server_url} is correct and "
+                      f"the repository '{repo_name}' actually exists?")
+                abort(3)
+
+            continuation_token = resp["continuationToken"]
+            asset_listing += resp["items"]
+
+            pbar.update()
+
+    return asset_listing
+
+
+def download_assets(output_dir, no_verify, asset_listing):
+    with tqdm(asset_listing) as pbar:
+        for asset in pbar:
+            file_path = os.path.join(output_dir, asset["path"])
+            error = download_single_asset(file_path, no_verify, asset)
+
+            if error:
+                pbar.close()
+                print(f"Failed downloading '{file_path}' due to the following error:")
+                print(error)
+                abort(4)
+
+
+def download_single_asset(file_path, no_verify, asset):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    for tryy in range(1, 11):
+        try:
+            r = requests.get(asset["downloadUrl"], verify=False)
+            with open(file_path, "wb") as f:
+                f.write(r.content)
+        except IOError as e:
+            # The requests API tries multiple times internally, so if it can't connect, the connection is probably down.
+            return str(e)
+
+        if no_verify:
+            tqdm.write(f"Downloaded '{file_path}' (not verified!)")
+            return False
+        elif asset["checksum"]["sha1"] == sha1(file_path):
+            tqdm.write(f"Downloaded and verified '{file_path}' (try {tryy})")
+            return False
+        else:
+            tqdm.write(f"SHA-1 Verification failed on '{file_path}' (try {tryy}); retrying...")
+
+    # If, after 10 tries, the SHA-1 hash is still wrong, something's probably corrupted.
+    return "Repeated SHA-1 verification failure"
+
+
+def sha1(file_path):
+    with open(file_path, "rb") as f:
+        return hashlib.sha1(f.read()).hexdigest()
+
+
+if __name__ == "__main__":
+    main()
